866b4595db7310d81cc19a209a35bb63a3bfb759
Dev/Junior_SilasJunior <silasfigueredodasilvajunior@gmail.com>
2025-04-24 23:12:28 -0300

    mais arquivos

---
+++ jogo01/game.cpp
@@ -0,0 +1,12 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    cout << "AÌ sim" << endl;
+    return 0;
+    
+}
